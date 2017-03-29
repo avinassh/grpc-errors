28bf77c28b2b62029d797e06b24880869c243af3
avi <hi@avi.im>
2017-03-29 17:59:26 +0530

    Add HelloClient and HelloServer projects

---
+++ csharp/Hello/HelloClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloClient
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ csharp/Hello/HelloServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloServer
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
