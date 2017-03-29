5f174f8fb3ab8d90ac949b3f0d6c7270c2c0981f
avi <hi@avi.im>
2017-03-29 15:57:28 +0530

    Add failing case and handle errors

--- swift/HelloClient/HelloClient/ViewController.swift
+++ swift/HelloClient/HelloClient/ViewController.swift
@@ -35,7 +35,30 @@ class ViewController: UIViewController {
         
         request.name = "Euler"
         client.sayHelloWithRequest(request, handler: {(response: HelloResp?, error: NSError?) in
-            print("\(response!.result)")
+            // ideally you should check for errors here too
+            print(response!.result)
+        })
+        
+        // lets try the failing case
+        request.name = "Leonhard Euler"
+        client.sayHelloStrictWithRequest(request, handler: { (response: HelloResp?, error: NSError?) in
+            if let response = response {
+                print(response.result)
+            } else {
+                // ouch!
+                // lets print the gRPC error message
+                // which is "Length of `Name` cannot be more than 10 characters"
+                let error = error!
+                print(error.localizedDescription)
+                // Want its int version for some reason?
+                // you shouldn't actually do this, but if you need for debugging,
+                // you can access `error.code` which will give you `3`
+                print(error.code)
+                // Want to take specific action based on specific error?
+                if (Int32(error.code) == GRPC_STATUS_INVALID_ARGUMENT.rawValue) {
+                    // do your thing
+                }
+            }
         })
     }
 }
