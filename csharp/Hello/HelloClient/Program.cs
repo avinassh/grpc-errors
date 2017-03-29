5f9d2ebf02326f505a2a3c4c476659aed225c172
avi <hi@avi.im>
2017-03-29 19:48:51 +0530

    Add client for simple call

--- csharp/Hello/HelloClient/Program.cs
+++ csharp/Hello/HelloClient/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using Grpc.Core;
+using Hello;
 
 namespace HelloClient
 {
-	class MainClass
+	class Program
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+
+			var client = new HelloService.HelloServiceClient(channel);
+			String user = "Euler";
+
+			var reply = client.SayHello(new HelloReq { Name = user });
+			Console.WriteLine(reply.Result);
+
+			channel.ShutdownAsync().Wait();
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
 		}
 	}
 }
