11dee22f820dadeb5920d474d6f390183d0be5d7
avi <hi@avi.im>
2017-03-29 19:41:06 +0530

    Implement `SayHelloStrict`

--- csharp/Hello/HelloServer/Program.cs
+++ csharp/Hello/HelloServer/Program.cs
@@ -7,10 +7,18 @@ namespace HelloServer
 {
 	class HelloServerImpl : HelloService.HelloServiceBase
 	{
-		// Server side handler of the SayHello RPC
 		public override Task<HelloResp> SayHello(HelloReq request, ServerCallContext context)
 		{
-			return Task.FromResult(new HelloResp { Result = "Hello " + request.Name });
+			return Task.FromResult(new HelloResp { Result = "Hey " + request.Name });
+		}
+
+		public override Task<HelloResp> SayHelloStrict(HelloReq request, ServerCallContext context)
+		{
+			if (request.Name.Length >= 10) {
+				const string msg = "Length of `Name` cannot be more than 10 characters";
+				throw new RpcException(new Status(StatusCode.InvalidArgument, msg));	
+			}
+			return Task.FromResult(new HelloResp { Result = "Hey " + request.Name });
 		}
 	}
 
@@ -26,10 +34,10 @@ namespace HelloServer
 				Services = { HelloService.BindService(new HelloServerImpl()) },
 				Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
 			};
+
 			server.Start();
 
-
-			Console.WriteLine("Greeter server listening on port " + Port);
+			Console.WriteLine("Hello server listening on port " + Port);
 			Console.WriteLine("Press any key to stop the server...");
 			Console.ReadKey();
 
