3cd7b29106018be74af6e57c7ee79472053b196a
avi <hi@avi.im>
2017-03-29 14:39:00 +0530

    Initial Import: Default iOS Swift app

---
+++ swift/HelloClient/HelloClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloClient
+//
+//  Created by avi on 29/03/17.
+//  Copyright © 2017 avi.im. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
