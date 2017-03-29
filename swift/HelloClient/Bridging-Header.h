25defb9a07aa51ff06cf72dc20795dc599d3841a
avi <hi@avi.im>
2017-03-29 15:15:30 +0530

    Add bridging header

---
+++ swift/HelloClient/Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging-Header.h
+//  
+//
+//  Created by avi on 29/03/17.
+//
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <GRPCClient/GRPCCall+ChannelArg.h>
+#import <GRPCClient/GRPCCall+Tests.h>
+#import <Hello/Hello.pbrpc.h>
+
+#endif /* Bridging_Header_h */
