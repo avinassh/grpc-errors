3718277e10f8fff0b98e7081ac486586f9c5d67f
avi <hi@avi.im>
2017-03-29 19:35:52 +0530

    Add basic hello server version

--- csharp/Hello/HelloServer/Program.cs
+++ csharp/Hello/HelloServer/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Hello;
 
 namespace HelloServer
 {
-	class MainClass
+	class HelloServerImpl : HelloService.HelloServiceBase
 	{
+		// Server side handler of the SayHello RPC
+		public override Task<HelloResp> SayHello(HelloReq request, ServerCallContext context)
+		{
+			return Task.FromResult(new HelloResp { Result = "Hello " + request.Name });
+		}
+	}
+
+	class Program
+	{
+		const int Port = 50051;
+
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+
+			Server server = new Server
+			{
+				Services = { HelloService.BindService(new HelloServerImpl()) },
+				Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+			};
+			server.Start();
+
+
+			Console.WriteLine("Greeter server listening on port " + Port);
+			Console.WriteLine("Press any key to stop the server...");
+			Console.ReadKey();
+
+			server.ShutdownAsync().Wait();
 		}
 	}
 }
